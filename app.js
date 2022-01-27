a348ab92a3f38f55c3bdaba70f9811087f978eef
James Epstein <je451@live.mdx.ac.uk>
2022-01-27 15:22:42 +0000

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 // parse the request parameters
-server.use(cors())
+app.use(cors())
 app.use(express.json())
 
 var path = require("path");
